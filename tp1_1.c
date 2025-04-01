c097ae51c19a2ed24b2bc6def3629c40f5fd3e2b
Gonzalo <gonzaloibrahim00@gmail.com>
2025-03-31 23:33:41 -0300

    Ejercicio 3

--- tp1_1.c
+++ tp1_1.c
@@ -3,7 +3,15 @@
 
 int main(){
     
-    printf("Hola mundo");
+    int *punt, edad = 20;
+    punt = &edad;
+    printf("Hola mundo\n");
+    printf("Contenido del puntero:%d",*punt);
+    printf("\nDireccion de memoria almacenada por el puntero:%p",punt);
+    printf("\nLa direccion de memoria de la variable:%p",&edad);
+    printf("\nLa direccion de memoria del puntero es:%p",&punt);
+    printf("\nTamanio de memoria ocupada por la variable:%d",sizeof(int));
+
 
 
     return 0;
